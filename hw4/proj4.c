db298a9b4c348e151ff07f1a43ab7e5428f56590
Will O'Neill <williamoneill1738@gmail.com>
2025-10-01 17:53:43 -0400

    Finished the main I/O for hw4 as well as the lru fifo and min policies Just barely started clock policy

---
+++ hw4/proj4.c
@@ -0,0 +1,402 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_NUM_REFERENCES 1024
+#define MIN_NUM_FRAMES 3
+#define MAX_NUM_FRAMES 20
+
+typedef struct Frame
+{
+	int page;
+	int num_calls_since_last_called;
+	int reference_bit;
+} Frame;
+
+void lru(int num_frames, int* references, bool trace);
+void fifo(int num_frames, int* references, bool trace);
+void min(int num_frames, int* references, bool trace);
+void clock(int num_frames, int* references, bool trace);
+void lfu(int num_frames, int* references, bool trace);
+
+void print_trace(int reference, bool found, Frame** frames, int num_frames);
+
+int main(int argc, char* argv[])
+{
+	if (argc != 4 && argc != 5)
+	{
+		printf("Usage: ./proj4 <policy> <num frames> <reference string filename> <Optional: trace>\n");
+		return 1;
+	}
+
+	bool trace = false;
+
+	if (argc == 5)
+	{
+		if (!strcmp(argv[4], "trace"))
+		{
+			trace = true;
+		}
+	}
+
+	int num_frames = atoi(argv[2]);
+
+	if (num_frames < MIN_NUM_FRAMES || num_frames > MAX_NUM_FRAMES)
+	{
+		printf("Number of frames cannot be less than 3 or greater than 20.\n");
+		return 1;
+	}
+
+	FILE* f = fopen(argv[3], "r");
+
+	if (f == NULL)
+	{
+		printf("Could not open file.\n");
+		return 1;
+	}
+	
+	char buf[500];
+	int* references = malloc(sizeof(int) * MAX_NUM_REFERENCES);
+	int i = 0;
+
+	while (fgets(buf, 500, f) > 0)
+	{
+		char* ptr = &buf[0];
+		int bytes_read;
+		while (ptr[0] != '\0' && sscanf(ptr, "%d%n", &references[i], &bytes_read) == 1)
+		{
+			ptr += bytes_read;
+			i++;
+		}
+	}
+
+	references[i] = -1;
+
+	if (!strcmp("lru", argv[1]))
+	{
+		lru(num_frames, references, trace);
+	}
+	else if (!strcmp("fifo", argv[1]))
+	{
+		fifo(num_frames, references, trace);
+	}
+	else if (!strcmp("min", argv[1]))
+	{
+		min(num_frames, references, trace);
+	}
+	else if (!strcmp("clock", argv[1]))
+	{
+		clock(num_frames, references, trace);
+	}
+	else if (!strcmp("lfu", argv[1]))
+	{
+		lfu(num_frames, references, trace);
+	}
+	else
+	{
+		printf("Invalid policy.\n");
+		free(references);
+		return 1;
+	}
+
+	free(references);
+	return 0;
+}
+
+void lru(int num_frames, int* references, bool trace)
+{
+	Frame** frames = malloc(sizeof(Frame*) * num_frames);
+
+	for (int i = 0; i < num_frames; i++)
+	{
+		frames[i] = malloc(sizeof(Frame));
+		frames[i]->page = -1;
+	}
+
+	int i = 0;
+	int num_page_faults = 0;
+	while (references[i] != -1)
+	{
+		bool found = false;
+
+		int j = 0;
+		for (; j < num_frames; j++)
+		{
+			if (frames[j]->page == references[i])
+			{
+				found = true;
+				frames[j]->num_calls_since_last_called = 0;
+				break;
+			}
+			if (frames[j]->page == -1)
+			{
+				break;
+			}
+			frames[j]->num_calls_since_last_called++;
+		}
+
+		if (!found)
+		{
+			if (j < num_frames)
+			{
+				frames[j]->page = references[i];
+				frames[j]->num_calls_since_last_called = 0;
+			}
+			else
+			{
+				int index = 0;
+
+				for (int j = 1; j < num_frames; j++)
+				{
+					if (frames[j]->num_calls_since_last_called > frames[index]->num_calls_since_last_called)
+					{
+						index = j;
+					}
+				}
+
+				frames[index]->page = references[i];
+				frames[index]->num_calls_since_last_called = 0;
+			}
+
+			num_page_faults++;
+		}
+
+		if (trace)
+		{
+			print_trace(references[i], found, frames, num_frames);
+		}
+
+		i++;
+	}
+
+	printf("lru policy with %d frames: %d page faults for %d references\n", num_frames, num_page_faults, i);
+
+	for (int i = 0; i < num_frames; i++)
+	{
+		free(frames[i]);
+	}
+
+	free(frames);
+}
+
+void fifo(int num_frames, int* references, bool trace)
+{
+	Frame** frames = malloc(sizeof(Frame*) * num_frames);
+
+	for (int i = 0; i < num_frames; i++)
+	{
+		frames[i] = malloc(sizeof(Frame));
+		frames[i]->page = -1;
+	}
+
+	int i = 0;
+	int num_page_faults = 0;
+	int next_to_remove = 0;
+
+	while (references[i] != -1)
+	{
+		bool found = false;
+
+		int j = 0;
+		for (; j < num_frames; j++)
+		{
+			if (references[i] == frames[j]->page)
+			{
+				found = true;
+				break;
+			}
+			if (frames[j]->page == -1)
+			{
+				break;
+			}
+		}
+
+		if (!found)
+		{
+			if (j < num_frames)
+			{
+				frames[j]->page = references[i];
+			}
+			else
+			{
+				frames[next_to_remove]->page = references[i];
+				next_to_remove++;
+				if (next_to_remove == num_frames)
+				{
+					next_to_remove = 0;
+				}
+			}
+			
+			num_page_faults++;
+		}
+
+		if (trace)
+		{
+			print_trace(references[i], found, frames, num_frames);
+		}
+
+		i++;
+	}
+
+	printf("fifo policy with %d frames: %d page faults for %d references\n", num_frames, num_page_faults, i);
+
+	for (int i = 0; i < num_frames; i++)
+	{
+		free(frames[i]);
+	}
+
+	free(frames);
+}
+
+void min(int num_frames, int* references, bool trace)
+{
+	Frame** frames = malloc(sizeof(Frame*) * num_frames);
+
+	for (int i = 0; i < num_frames; i++)
+	{
+		frames[i] = malloc(sizeof(Frame));
+		frames[i]->page = -1;
+	}
+
+	int i = 0;
+	int num_page_faults = 0;
+
+	while (references[i] != -1)
+	{
+		bool found = false;
+
+		int j = 0;
+		for (; j < num_frames; j++)
+		{
+			if (references[i] == frames[j]->page)
+			{
+				found = true;
+				break;
+			}
+			if (frames[j]->page == -1)
+			{
+				break;
+			}
+		}
+
+		if (!found)
+		{
+			if (j < num_frames)
+			{
+				frames[j]->page = references[i];
+			}
+			else
+			{
+				int index;
+				int how_far_away = -1;
+				for (j = 0; j < num_frames; j++)
+				{
+					int k = i + 1;
+					bool found2 = false;
+
+					// highly inefficent but whatever
+					// better implementation would be to maintain how far away each frame is from its next reference,
+					// then decrement that value every time a reference is made, then take the highest of those values
+					// every time a replacement is necessary
+					for (; references[k] != -1; k++)
+					{
+						if (references[k] == frames[j]->page)
+						{
+							found2 = true;
+
+							if (k - i - 1 > how_far_away)
+							{
+								how_far_away = k - i - 1;
+								index = j;
+							}
+
+							break;
+						}
+					}
+
+					if (!found2)
+					{
+						index = j;
+						break;
+					}
+				}
+
+				frames[index]->page = references[i];
+			}
+
+			num_page_faults++;
+		}
+
+		if (trace)
+		{
+			print_trace(references[i], found, frames, num_frames);
+		}
+
+		i++;
+	}
+
+	printf("min policy with %d frames: %d page faults for %d references\n", num_frames, num_page_faults, i);
+
+	for (int i = 0; i < num_frames; i++)
+	{
+		free(frames[i]);
+	}
+
+	free(frames);
+}
+
+void clock(int num_frames, int* references, bool trace)
+{
+	Frame** frames = malloc(sizeof(Frame*) * num_frames);
+
+	for (int i = 0; i < num_frames; i++)
+	{
+		frames[i] = malloc(sizeof(Frame));
+		frames[i]->page = -1;
+	}
+
+	int i = 0;
+	int num_page_faults = 0;
+	int index = 0;
+
+	while (references[i] != -1)
+	{
+
+	}
+}
+
+void lfu(int num_frames, int* references, bool trace)
+{
+
+}
+
+void print_trace(int reference, bool found, Frame** frames, int num_frames)
+{
+	printf("%d ", reference);
+
+	if (found)
+	{
+		printf("- ");
+	}
+	else
+	{
+		printf("P ");
+	}
+
+	printf("Frames:");
+
+	int j;
+
+	for (j = 0; j < num_frames && frames[j]->page != -1; j++)
+	{
+		printf(" %d", frames[j]->page);
+	}
+
+	for (; j < num_frames; j++)
+	{
+		printf(" -");
+	}
+
+	printf("\n");
+}
